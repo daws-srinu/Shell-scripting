592a7c4f01eb3d21e1822cf4ff25c4a424d70ec2
Srinivasa Rao <sanapathisrinivasarao953@gmail.com>
2025-11-24 21:17:58 +0530

    Variables add chesa

--- 03-Variables.sh
+++ 03-Variables.sh
@@ -4,7 +4,7 @@ PERSON1=Raju  #no space before and after equal
 PERSON2=Deva
 
 
-echo "$PERSION1 :: Hi $PERSON2"
-echo "$PERSION2 :: Hello $PERSON1"
+echo "$PERSON1 :: Hi $PERSON2"
+echo "$PERSON2 :: Hello $PERSON1"
 echo "$PERSON1 : Hello $PERSON2 how are you doing "
-echo "$PERSION2 : I am good .How are you " 
+echo "$PERSON2 : I am good .How are you " 
