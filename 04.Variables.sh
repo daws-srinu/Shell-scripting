88696104af3339e2f04424081ced72f456588565
Srinivasa Rao <sanapathisrinivasarao953@gmail.com>
2025-11-24 21:27:40 +0530

    Args add chestha ela chayali ani in plase off names we put  and

---
+++ 04.Variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1=$1   # we put some arguements or args no space before and after equal
+PERSON2=$2 
+
+
+echo "$PERSON1 :: Hi $PERSON2"
+echo "$PERSON2 :: Hello $PERSON1"
+echo "$PERSON1 : Hello $PERSON2 how are you doing "
+echo "$PERSON2 : I am good .How are you " 
