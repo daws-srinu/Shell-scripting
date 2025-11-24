701d3c7f183360232200b48819289ebb53632c97
Srinivasa Rao <sanapathisrinivasarao953@gmail.com>
2025-11-24 21:12:25 +0530

    variables added with person1 and person2

---
+++ 03-Variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1=Raju  #no space before and after equal
+PERSON2=Deva
+
+
+echo "$PERSION1 :: Hi "$PERSON2"
+echo "$PERSION2 :: Hello "$PERSON1"
+echo "$PERSON1 : Hello $PERSON2" how are you doing "
+echo "$PERSION2 : I am good .How are you " 
