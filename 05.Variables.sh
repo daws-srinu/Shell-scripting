5f4faa86cbd11e8b89fbc79c06a6a5c3f620f396
Srinivasa Rao <sanapathisrinivasarao953@gmail.com>
2025-11-24 21:46:46 +0530

    passwords ni and username ela dispaly chayali ani

---
+++ 05.Variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Please enter your username ::" # text enetered here will be added as valus to variable
+read -s USERNAME # Here username is the variable name
+echo "username entered: $USERNAME"
+
+echo " Please enter your password :: "
+
+read -s PASSWORD
